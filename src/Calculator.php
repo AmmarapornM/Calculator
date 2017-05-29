b0df751ebd912d3613c8c0f8e1b909603b246a2d
AmmarapornM <ammarapornm57@email.nu.ac.th>
2017-05-29 15:42:32 +0700

    Initial commit

---
+++ src/Calculator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AmmaraoprnM\Calculator;
+
+class Calculator
+{
+   public function add($first_number, $second_number) 
+    {
+        return $first_number + $second_number;
+    }
+    public function subtract($first_number, $second_number)
+    {
+        return $first_number - $second_number;
+    }
+    public function mutiply($first_number, $second_number)
+    {
+        return $first_number * $second_number;
+    }
+    public function divide($first_number, $second_number)
+    {
+        return $first_number / $second_number;
+    }
+}
+
